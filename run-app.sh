69121fc14200d7f9822ff69ff19a535d8b342fce
Renan Consalter <renan.consalter@gmail.com>
2021-05-13 09:03:34 -0300

    ci(script): make a script shell to run Docker using multistage builds

---
+++ run-app.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $1 == "--dev" ]; then
+    echo "Starting development environment..."
+    echo "Removing old containers..."
+    docker compose down
+    echo "Building containers in development environment..."
+    docker compose up -d --build
+fi
+
+if [ $1 == "--prod" ]; then
+    echo "Starting deploy to production environment..."
+    echo "Removing old containers..."
+    docker compose -f docker-compose-prod.yml down
+    echo "Building containers in production environment..."
+    docker compose -f docker-compose-prod.yml up -d --build
+fi
