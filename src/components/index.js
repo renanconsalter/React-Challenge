8f96246c520c76164ccecf7ddb5b61b0071d5d0c
Renan Consalter <renan.consalter@gmail.com>
2021-05-13 00:27:28 -0300

    feat(components): exports all previously created components

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Header } from './Header';
+export { default as Banner } from './Banner';
+export { default as Footer } from './Footer';
+export { default as PlansContainer } from './PlansContainer';
+export { default as PlanCard } from './PlanCard';
+export { default as PeriodSelector } from './PeriodSelector';
+export { default as Button } from './Button';
+export { default as Carousel } from './Carousel';
