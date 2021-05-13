eec4c7e99a33adfc6eed7466123baada24d22f2d
Renan Consalter <renan.consalter@gmail.com>
2021-05-13 00:01:55 -0300

    feat: modifies the project structure to create-react-app

--- pages/_app.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import wrapper from '../redux/store';
-
-const MyApp = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
-);
-
-export default wrapper.withRedux(MyApp);
